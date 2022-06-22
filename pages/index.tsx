a6fca7d7ccb5b19c5000a695d796625f11a05228
daduckster <vlasak.o@yandex.ru>
2022-06-22 15:46:39 +0200

    Updated the main component to correctly receive and convert the user's integer to a roman numeral. The input field now takes the value from userInput in state. Added onChange (handleUserInputChange) and onSubmit(handleConvert) functions.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import Head from 'next/head'
+import {convertToRoman} from "../utils/calculatorHelper";
+import {number} from "prop-types";
 
 export default function Home() {
   const [calculatedRomanNumber, setCalculatedRomanNumber] = useState<string>('');
+  const [userInput, setUserInput] = useState<string>('');
+
+  function handleUserInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+   setUserInput(e.target.value);
+  }
+
+  function handleConvert(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setCalculatedRomanNumber(convertToRoman(userInput));
+  }
 
   return (
     <div>
@@ -12,9 +24,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <form>
+        <form onSubmit={handleConvert}>
           <label htmlFor="user-input">Your Number
-            <input type="text" id="user-input"/>
+            <input type="text" id="user-input" value={userInput} onChange={handleUserInputChange}/>
           </label>
           <input type="submit" value="Convert"/>
         </form>
