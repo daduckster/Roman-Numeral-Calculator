327228baf8062e512793d966f7cb18bb284f769a
daduckster <vlasak.o@yandex.ru>
2022-06-22 16:00:30 +0200

    Created testing cases to check if the component renders output correctly. All tests are passing. Also tried multiple invalid inputs in a browser. It throws an error in console

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import Home from '@/pages/index'
 
 describe('Home', () => {
@@ -15,5 +15,23 @@ describe('Home', () => {
   it('renders the output heading', () => {
     render(<Home />);
     expect(screen.getByText('Your Roman Numeral')).toBeInTheDocument();
+  })
+
+  it('shows DXII in the output when the number is 512', () => {
+    render(<Home />);
+    const input = screen.getByLabelText('Your Number');
+    const button = screen.getByText('Convert');
+    fireEvent.change(input, {target: {value: 512}});
+    fireEvent.click(button)
+    expect(screen.getByText('DXII')).toBeInTheDocument();
+  })
+
+  it('shows DCCCLVII in the output when the number is 857', () => {
+    render(<Home />);
+    const input = screen.getByLabelText('Your Number');
+    const button = screen.getByText('Convert');
+    fireEvent.change(input, {target: {value: 857}});
+    fireEvent.click(button)
+    expect(screen.getByText('DCCCLVII')).toBeInTheDocument();
   })
 })
